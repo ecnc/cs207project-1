682e664dfb434cc8a557d98983045e3aa83a6dfd
Qin Lyu <qlyu@g.harvard.edu>
2016-10-14 19:39:11 -0400

    style modification

--- timeseries/ts.py
+++ timeseries/ts.py
@@ -1,31 +1,23 @@
-import reprlib 
-
+import reprlib
 class TimeSeries:
-  """
-  Implementation of TimeSeries.
-  
-  TODO:
-  1. Detailed documentation, including docstrings.
-  2. Add tests. 
-  
-  """
-  def __init__(self, data = None):
-    self._data = []
-    if data:
-      for d in data: self._data.append(d) 
-
-  def __len__(self):
-    return len(self._data)
-
-  def __getitem__(self, key):
-    return self._data[key]
-    
-
-  def __setitem__(self, key, value):
-    self._data[key] = value
-    return True
-
-  def __repr__(self):
-    className = type(self).__name__
-    return "%s(%s)" % (className, reprlib.repr(self._data))
-  
+    """
+    Implementation of TimeSeries.
+    TODO:
+    1. Detailed documentation, including docstrings.
+    2. Add tests.
+    """
+    def __init__(self, data=None):
+        self._data = []
+        if data:
+            for item in data:
+                self._data.append(item)
+    def __len__(self):
+        return len(self._data)
+    def __getitem__(self, key):
+        return self._data[key]
+    def __setitem__(self, key, value):
+        self._data[key] = value
+        return True
+    def __repr__(self):
+        class_name = type(self).__name__
+        return "%s(%s)" % (class_name, reprlib.repr(self._data))
