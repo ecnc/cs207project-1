1fd27149d0e8865d14bc9dbec59a95cacf162d8d
minglong-cse2016 <minglong_wu@g.harvard.edu>
2016-10-23 23:32:33 -0400

    update partI attempt6

--- timeseries/test_ts.py
+++ timeseries/test_ts.py
@@ -1,26 +1,21 @@
 from ts import TimeSeries
 from pytest import raises
 
-# test input with zero length
 def test_zerolen():
     with raises(ValueError):
         TimeSeries([])
 
-# test non-int or non-float input
 def test_wrongtype():
     with raises(TypeError):
         TimeSeries([1, 2, 'hi'])
 
-# test case with no input
 def test_noninput():
     with raises(TypeError):
         TimeSeries()
 
-# test typical input
 def test_typical():
     assert TimeSeries([1, 2, 3]) == str('TimeSeries([1, 2, 3])')
 
-# test typical print
 def test_typical_print():
     assert print(TimeSeries([1, 2, 3]) == str('[1, 2, 3], length = 3')
 
