d5225a94486bd90822e2abd75e02f2a8146ffd3d
minglong-cse2016 <minglong_wu@g.harvard.edu>
2016-10-24 00:21:25 -0400

    update partI attempt10

--- timeseries/test_ts.py
+++ timeseries/test_ts.py
@@ -13,3 +13,14 @@ def test_noninput():
     with raises(TypeError):
         TimeSeries()
 
+def test_typical_input_repr():
+    assert repr(TimeSeries([1, 2, 3])) == 'TimeSeries([1, 2, 3])'
+
+def test_typical_input_str():
+    assert str(TimeSeries([1, 2, 3])) == '[1, 2, 3], length = 3'
+
+def test_long_input_repr():
+    assert repr(TimeSeries(range(10))) == 'TimeSeries([0, 1, 2, 3, 4, 5, ...])'
+
+def test_long_input_str():
+    assert str(TimeSeries(range(10))) == '[0, 1, 2, 3, 4, 5, ...], length = 10'
