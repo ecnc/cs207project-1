d745c4cd965c7497f07268d4a15bfa7e014e05bc
MelodyNymph <ruz466@g.harvard.edu>
2016-10-26 16:12:42 -0400

    add class: array_timeseries

--- timeseries/ts.py
+++ timeseries/ts.py
@@ -11,13 +11,17 @@ class TimeSeries:
         if data:
             for item in data:
                 self._data.append(item)
+                
     def __len__(self):
         return len(self._data)
+    
     def __getitem__(self, key):
         return self._data[key]
+    
     def __setitem__(self, key, value):
         self._data[key] = value
         return True
+    
     def __repr__(self):
         class_name = type(self).__name__
         return "%s(%s)" % (class_name, reprlib.repr(self._data))
@@ -33,4 +37,23 @@ class TimeSeries:
     def iteritems(self):
         for i,item in enumerate(self._data):
               yield (i,item)
-    	
+    
+    
+import numpy as np 
+class ArrayTimeSeries(TimeSeries):
+    """
+    Implementation of ArrayTimeSeries.
+    TODO:
+    1. Detailed documentation, including docstrings.
+    2. Add tests.
+    """
+    def __init__(self, data):
+        if data:
+            self._data = np.array(data)
+        self._data = np.array([])    
+            
+   
+    
+    
+    
+
