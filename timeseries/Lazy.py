71ea67d9753b04248b569954af46eb9314f083b4
MelodyNymph <ruz466@g.harvard.edu>
2016-10-31 01:50:20 -0400

    add doc to lazy.py

--- timeseries/Lazy.py
+++ timeseries/Lazy.py
@@ -1,25 +1,42 @@
 class LazyOperation:
     """
-    LazyOperation
+    This is a class which functions as thunk.
 
-    TODO:
-    1. how to test lazy
-    1. test;
-    2. documatation;
-
+    Methods
+    -------
+    eval(self):
+        Recursively evaluate lazy arguments
+    create_thunk(*args, **kwargs):
+        create thunk for the @lazy-decorated functions
+ 
     """
+
     def __init__(self, function, *args, **kwargs):
+        “””
+        Constructor
+ 
+        Input: one required argument function and arbitrary positional
+        and keyword arguments
+        The constructor stores them internally
+        “””
         self._function = function
         self._args = args
         self._kwargs = kwargs
 
     def eval(self):
-        # Recursively eval() lazy args
+        “””
+        Recursively evaluate lazy arguments
+        Return function with the arguments
+        “””
         new_args = [a.eval() if isinstance(a, LazyOperation) else a for a in self._args]
         new_kwargs = {k: v.eval() if isinstance(v, LazyOperation) else v for k, v in self._kwargs}
         return self._function(*new_args, **new_kwargs)
 
 def lazy(function):
     def create_thunk(*args, **kwargs):
+        “””
+        Produce chunk for the @lazy-decorated function
+        “””
         return LazyOperation(function, *args, **kwargs)
     return create_thunk
+
