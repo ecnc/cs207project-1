c930dabdaf111e593a2d2c6f4e34b6e6b11a16ac
minglong-cse2016 <minglong_wu@g.harvard.edu>
2016-10-31 00:30:37 -0400

    add doc for SizedContainerTimeSeriesInterface and StreamTimeSeriesInterface

--- timeseries/Interface.py
+++ timeseries/Interface.py
@@ -1,10 +1,25 @@
+""" Interface module for TimeSeries.
+This module defines abstract interface classes for TimeSeries.
+
+Classes
+-------
+TimeSeriesInterface(abc.ABC):
+    Define a class for all timeseries-related classes
+SizedContainerTimeSeriesInterface(TimeSeriesInterface):
+    Define a class for container-like timeseries
+StreamTimeSeriesInterface(TimeSeriesInterface):
+    Define a class for stream-like timeseries
+
+"""
+
 import abc
 import reprlib
 import numpy as np
 
+
 class TimeSeriesInterface(abc.ABC):
     """ Interface class for TimeSeries.
-    The is an abstract interface class for timeseries-related classes.
+    The is an abstract interface class for all timeseries-related classes.
     Common methods that will be used in timeseries-related classes are defined.
     
     Methods
@@ -62,7 +77,7 @@ class SizedContainerTimeSeriesInterface(TimeSeriesInterface):
     Methods
     -------
     __setitem__(self, index, value):
-        Set the value at the postion index by value
+        Set the value at the postion index
     __add__(self, other):
         Return self + other
     __sub__(self, other):
@@ -80,12 +95,34 @@ class SizedContainerTimeSeriesInterface(TimeSeriesInterface):
     __pos__(self):
         Return pos(self)
     interpolate(self, time_seq):
-        Return a new timeseries that is a interpolation of current timeseries, with time stamps given by time_seq
+        Return a new timeseries that is a interpolation of current timeseries, 
+        with time stamps given by time_seq
     mean(self):
         Return mean of self._value
     std(self):
         Return standard deviation of self._value
-
+    __len__(self):
+        Return length of self._value
+    __getitem__(self, index):
+        Return the value of self._timeseries at the position index
+    __iter__(self):
+        Iterate over self._value
+    __contains__(self, val):
+        Return True if val is in self._value, False otherwise
+    values(self):
+        Return self._value
+    times(self):
+        Return self._time
+    items(self):
+        Return self._timeseries
+    itervalues(self):
+        Iterate over self._value
+    itertimes(self):
+        Iterate over self._time
+    __str__(self):
+        Format (informal) string output of class
+    __repr__(self):
+        Format (official) string output of class
     
     """
 
@@ -133,11 +170,13 @@ class SizedContainerTimeSeriesInterface(TimeSeriesInterface):
         """
         Return bool(self)
         """
+
     @abc.abstractmethod
     def __neg__(self):
         """
         Return neg(self)
         """
+
     @abc.abstractmethod
     def __pos__(self):
         """
@@ -152,65 +191,122 @@ class SizedContainerTimeSeriesInterface(TimeSeriesInterface):
         """
 
     def mean(self):
-        return np.mean(self._value)
         """
         Return mean of self._value
         """
+        return np.mean(self._value)
+
 
     def std(self):
-        return np.std(self._value)
         """
         Return standard deviation of self._value
         """
+        return np.std(self._value)
+
 
     def __len__(self):
+        """
+        Return length of self._value
+        """
         return len(self._value)
 
+
     def __getitem__(self, index):
+        """
+        Return the value of self._timeseries at the position index
+        """
         return self._timeseries[index]
 
+
     def __iter__(self):
+        """
+        Iterate over self._value
+        """
         for item in self._value:
             yield item
+
 
     def __contains__(self, val):
+        """
+        Return True if val is in self._value, False otherwise
+        """
         return val in self._value
 
+
     def values(self):
+        """
+        Return self._value
+        """
         return self._value
 
+
     def times(self):
+        """
+        Return self._time
+        """
         return self._time
 
+
     def items(self):
+        """
+        Return self._timeseries
+        """
         return self._timeseries
 
+
     def itervalues(self):
+        """
+        Iterate over self._value
+        """
         for item in self._value:
             yield item
 
+
     def itertimes(self):
+        """
+        Iterate over self._time
+        """
         for item in self._time:
             yield item
 
+ 
     def iteritems(self):
+        """
+        Iterate over time and value tuple, i.e., zip(_time, _value)
+        """
         for item in zip(self._time, self._value):
             yield item
 
 
     def __str__(self):
+        """
+        Format (informal) string output of class
+        """
         class_name = type(self).__name__
         return class_name + '(' + reprlib.repr([item for item in self._timeseries])
 
+
     def __repr__(self):
+        """
+        Format (official) string output of class
+        """
         class_name = type(self).__name__
         return class_name + '(' + reprlib.repr([item for item in self._timeseries]) + '), \
         length={}'.format(len(self))
 
-
 class StreamTimeSeriesInterface(TimeSeriesInterface):
-    """
-    Interface of stream-like timeseries
+    """ Interface class for StreamTimeSeries.
+    This is an abstract interface class for stream-like timeseries.
+    
+    Methods
+    -------
+    produce(self, chunk=1):
+        Produce a chunk sized bunch of new elements into the timeseries whenever it is called
+    online_mean(self):
+        Return a time series of means
+    online_std(self):
+        Return a time series of standard deviations
+    
     """
 
     @abc.abstractmethod
