821e11ebd2df1e7e43fba1320fc13c571d96a2e1
minglong-cse2016 <minglong_wu@g.harvard.edu>
2016-10-30 23:06:33 -0400

    added doc for TimeSeriesInterface

--- timeseries/Interface.py
+++ timeseries/Interface.py
@@ -3,42 +3,90 @@ import reprlib
 import numpy as np
 
 class TimeSeriesInterface(abc.ABC):
-    """
-    The abstract interface class for timeseries-related classes
+    """ Interface class for TimeSeries.
+    The is an abstract interface class for timeseries-related classes.
+    Common methods that will be used in timeseries-related classes are defined.
+    
+    Methods
+    -------
+    __str__(self):
+        Format (informal) string output of class
+    __repr__(self):
+        Format (official) string output of class
+    __iter__(self):
+        Iterate over values
+    itervalues(self):
+        Return an iterator of values
+    
     """
 
 
     @abc.abstractmethod
     def __str__(self):
         """
-        Format output of class (informal)
+        Format (informal) string output of class
         """
 
     @abc.abstractmethod
     def __repr__(self):
         """
-        Format output of class
+        Format (official) string output of class
         """
 
     @abc.abstractmethod
     def __iter__(self):
         """
-        Return an iterator
+        Iterate over values
         """
 
     @abc.abstractmethod
     def itervalues(self):
         """
-        Return iterator of values
+        Return an iterator of values
         """
 
 class SizedContainerTimeSeriesInterface(TimeSeriesInterface):
-    """
-    Interface of container-like timeseries
-    A SizedContainerTimeSeriesInterface instance should at least have the following variables:
-        self._time: store the times
-        self._value: store the values
-        self._timeseries: time and value tuple, i.e., zip(_time, _value)
+    """ Interface class for SizedContainerTimeSeries.
+    This is an abstract interface class for container-like timeseries.
+    
+    Attributes
+    ----------
+    A SizedContainerTimeSeriesInterface instance should at least have the following variables.
+    self._time:
+        store the times
+    self._value:
+        store the values
+    self._timeseries:
+        time and value tuple, i.e., zip(_time, _value)
+    
+    Methods
+    -------
+    __setitem__(self, index, value):
+        Set the value at the postion index by value
+    __add__(self, other):
+        Return self + other
+    __sub__(self, other):
+        Return self - other
+    __mul__(self, other):
+        Return self * other (element-wise)
+    __eq__(self, other):
+        Return self == other
+    __abs__(self):
+        Return abs(self)
+    __bool__(self):
+        Return bool(self)
+    __neg__(self):
+        Return neg(self)
+    __pos__(self):
+        Return pos(self)
+    interpolate(self, time_seq):
+        Return a new timeseries that is a interpolation of current timeseries, with time stamps given by time_seq
+    mean(self):
+        Return mean of self._value
+    std(self):
+        Return standard deviation of self._value
+
+    
     """
 
     @abc.abstractmethod
@@ -47,7 +95,7 @@ class SizedContainerTimeSeriesInterface(TimeSeriesInterface):
         Set the value at the position index by value
 
         This method should raise "LookupError" when the index is out of boundary,
-        and raise "TypeError" when the value is of illegal type
+        and should raise "TypeError" when the value is of illegal type.
         """
 
     @abc.abstractmethod
@@ -99,14 +147,21 @@ class SizedContainerTimeSeriesInterface(TimeSeriesInterface):
     @abc.abstractmethod
     def interpolate(self, time_seq):
         """
-        Return a new timeseries that is a interpolation of current timeseries, with time given by time_seq
+        Return a new timeseries that is a interpolation of current timeseries, \
+        with time stamps given by time_seq
         """
 
     def mean(self):
         return np.mean(self._value)
+        """
+        Return mean of self._value
+        """
 
     def std(self):
         return np.std(self._value)
+        """
+        Return standard deviation of self._value
+        """
 
     def __len__(self):
         return len(self._value)
@@ -149,7 +204,8 @@ class SizedContainerTimeSeriesInterface(TimeSeriesInterface):
 
     def __repr__(self):
         class_name = type(self).__name__
-        return class_name + '(' + reprlib.repr([item for item in self._timeseries]) + '), length={}'.format(len(self))
+        return class_name + '(' + reprlib.repr([item for item in self._timeseries]) + '), \
+        length={}'.format(len(self))
 
 
 class StreamTimeSeriesInterface(TimeSeriesInterface):
