7e0ef232e9807cd198165490b1a4d6b957f8117b
Xinyuan <wang_xinyuan@g.harvard.edu>
2016-11-01 09:02:22 -0400

    merge pull request(#6)
    * modified produce,online_mean, online_std functions for SimulatedTimeSeries
    
    * added doc for SimulatedTimeSeries

--- timeseries/SimulatedTimeSeries.py
+++ timeseries/SimulatedTimeSeries.py
@@ -4,51 +4,96 @@ from Interface import StreamTimeSeriesInterface
 
 class SimulatedTimeSeries(StreamTimeSeriesInterface):
     """
-    Stream-like timeseries without storage
+    A stream-like time series class without storage that implements StreamTimeSeriesInterface.
+
+    The class needs to take a generator, and could produce a chunk size of list of values each time. 
+    The timeseries is moving forward chunk steps after produce is called. It also implements 
+    online_mean and online_std functions, which both return SimulatedTimeSeries.
+
+    >>> data = (x for x in range(5))
+    >>> ts = SimulatedTimeSeries(data)
+    >>> ts.produce(3) 
+    [(0, 0), (1, 1), (2, 2)]
+    >>> ts.produce(3) 
+    [(3, 3), (4, 4)]
+    >>> data = (x for x in range(5))
+    >>> ts = SimulatedTimeSeries(data)
+    >>> ts_online_mean = ts.online_mean()
+    >>> ts_online_std = ts.online_std()
+    >>> ts_online_mean.produce(3) 
+    [(0, 0.0), (1, 0.5), (2, 1.0)]
+    >>> ts_online_std.produce(3) 
+    [(0, 0), (1, 0.7071067811865476)]
+
     """
     def __init__(self, gen):
+        """
+        Initialize a SimulatedTimeSeries instance, inherited from StreamTimeSeriesInterface
+
+        The input parameter gen should be a generator, otherwise it will print an error message 
+        to ask user to input a generator
+        self._gen: store the generator
+        self._time: record the current time
+        """
         if not isinstance(gen, types.GeneratorType):
             print("start printing")
             print(gen)
-            for v in gen: print(v)
+            for v in gen: 
+                print(v)
         assert isinstance(gen, types.GeneratorType), "SimulatedTimeSeries should be initialized by a generator"
         self._gen = gen
         self._time = 0
 
     def __str__(self):
+        """
+        Return class name and current time
+        """
         class_name = type(self).__name__
         return "{}(time={})".format(class_name, self._time)
 
     def __repr__(self):
+        """
+        Return class name and current time
+        """
         class_name = type(self).__name__
         return "{}(time={})".format(class_name, self._time)
 
     def __iter__(self):
+        """
+        Iterate over the generator
+        """
         yield from self._gen
 
     def itervalues(self):
+        """
+        Iterate over the generator
+        """
         yield from self._gen
 
     def produce(self, chunk=1):
+        """
+        Return a chunk size of list of values 
+
+        chunk must be an integer and is set as 1 by defult. Calling this function will move 
+        forward the time series chunk steps each time, but will stop when the generator reaches 
+        the end. self._time is changed correspondingly.
+        """
         values = []
-        while True:
-            while len(values) < chunk:
-                try:
-                    values.append((self._time, next(self._gen)))
-                except StopIteration:
-                    if values: yield values
-                    return
+        while len(values) < chunk:
+            try:
+                values.append((self._time, next(self._gen)))
                 self._time += 1
-            yield values
-            values = []
-
-
-    """
-    TODO: finish the online_mean and online_std function
-    """
+            except StopIteration:
+                return values
+        return values
+            
+ 
     def online_mean(self):
         """
         Return a SimulatedTimeSeries of means
+
+        The starting point of the series of means is the current starting point of the time series.
+        The mean generated each time is the mean of data from the starting time to current time.
         """
 
         def mean_generator():
@@ -59,13 +104,17 @@ class SimulatedTimeSeries(StreamTimeSeriesInterface):
                 delta = value - mu
                 mu = mu + delta / n
                 yield mu
-
-        ret = SimulatedTimeSeries(mean_generator)
+        mean_gen = mean_generator()
+        ret = SimulatedTimeSeries(mean_gen)
         return ret
 
     def online_std(self):
         """
         Return a SimulatedTimeSeries of standard deviations
+
+        The starting point of the series of standard deviations is the current starting point of 
+        the time series. The standard deviation generated each time is the standard deviation of 
+        data from the starting time to current time.
         """
 
         def std_generator():
@@ -81,6 +130,9 @@ class SimulatedTimeSeries(StreamTimeSeriesInterface):
                 if n > 1:
                     stddev = math.sqrt(dev_accum / (n - 1))
                 yield stddev
-
-        ret = SimulatedTimeSeries(std_generator)
+        std_gen = std_generator()
+        ret = SimulatedTimeSeries(std_gen)
         return ret
+
+
+
